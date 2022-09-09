b15e5b7fe10c885cdfc43710c9c504fb3c3c6249
Peter Njihia wambui <peternjihia2001@gmail.com>
2022-09-09 14:44:57 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
+
 /**
- * main - print if the number is postive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
- */
+  * main - Prints a hexadecimal string
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
-char ch;
-for (ch = 'a' ; ch <= 'z' ; ch++)
-{
-	putchar(ch);
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
-for (ch = 'A' ; ch <= 'Z' ; ch++)
-{
-	putchar(ch);
-}
-putchar('\n');
-return (0);
-}
+
