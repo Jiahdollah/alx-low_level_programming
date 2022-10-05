36b57c51ff5d9401ab33a3fed68f764b19d644e0
Jiahdollah <peternjihia2001@gmail.com>
2022-10-05 07:52:06 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth: peter njihia
+ */
+
 #include <stdio.h>
 
 /**
-* main - Printing
-*
-* Return: Always 0 (Successful)
-*/
+ * main - prints "Programming is like building a multilingual puzzle, followed by a new line.
+ *
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual
-puzzle");
-return (0);
+	puts("\"programing is like biulding a multilingual puzzel");
+
+	return (0);
 }
