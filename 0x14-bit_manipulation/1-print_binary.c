0a9b03c27fd42e50c5cd49066d6f906f5de81846
Jiahdollah <peternjihia2001@gmail.com>
2022-10-28 14:45:35 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,10 @@
-#include "mai.h"
+
+
+#include "holberton.h"
 #include <unistd.h>
 #include <limits.h>
 /**
- * print_binary - print an assigned int in binary
+ * print_binary - print an unsigned int in binary
  *
  * @n:
  *
@@ -10,22 +12,22 @@
  */
 void print_binary(unsigned long int n)
 {
-		unsigned long int printbit = 1ul << 63;
-			char c = '0';
+	unsigned long int printbit = 1ul << 63;
+	char c = '0';
 
-				while (!(printbit & n) && printbit != 0)
-							printbit = printbit >> 1;
+	while (!(printbit & n) && printbit != 0)
+		printbit = printbit >> 1;
 
-					if (printbit == 0)
-								write(1, &c, 1);
+	if (printbit == 0)
+		write(1, &c, 1);
 
-						while (printbit)
-								{
-											if (printbit & n)
-															c = '1';
-													else
-																	c = '0';
-															write(1, &c, 1);
-																	printbit = printbit >> 1;
-																		}
+	while (printbit)
+	{
+		if (printbit & n)
+			c = '1';
+		else
+			c = '0';
+		write(1, &c, 1);
+		printbit = printbit >> 1;
+	}
 }
