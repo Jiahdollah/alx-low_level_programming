2c1eef94f585ec56d3fddbaca273790488e7cd30
Jiahdollah <peternjihia2001@gmail.com>
2023-04-12 14:21:11 -0700

    Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,7 +16,7 @@ int linear_search(int *array, size_t size, int value)
     if (array == NULL)
         return (-1);
 
-    for (size_t i = 0; i < size; i++)
+    for (; i < size; ++i)
     {
         printf("Value checked array[%ld] = [%d]\n", i, array[i]);
         if (array[i] == value)
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef _BINARY_SEARCH_
+#define _BINARY_SEARCH_
+
+#include <stdlib.h>
+#include <stdio.h>
+/* Linear search algorithm */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+/* Binary search algorithm */
+#endif
